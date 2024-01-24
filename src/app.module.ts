7596354ca09fc7ab7b19e4bc74e6e3763013820c
91am <killaek.info@gmail.com>
2024-01-24 20:26:32 +0100

    configuracion de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 
 @Module({
-  imports: [],
+  imports: [ //sacado de la docu de techniques database
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', //Este es el usuario que se usa para acceder a la BD --yo lo tengo configurado como root, root--- dejar asi
+      password: 'root', //Ojo con esto por seguridad no hay datos personales 
+      database: 'test', // database es el nombre de la BD
+      entities: [], //AQUI HAY QUE AGREGAR CADA NUEVA ENTIDAD CREADA -- OJO!!
+      synchronize: true,
+    }), 
+     //AQUI HAY QUE REGISTRAR LOS MODULOS GENERADOS
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
